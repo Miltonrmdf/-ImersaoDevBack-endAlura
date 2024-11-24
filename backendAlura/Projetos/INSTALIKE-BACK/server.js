0957158150a049939d0998007f0e2341249a8aea
Miltonrmdf <miltonrmdf@gmail.com>
2024-11-24 18:45:52 -0300

    Segunda aula finalizada

--- backendAlura/Projetos/INSTALIKE-BACK/server.js
+++ backendAlura/Projetos/INSTALIKE-BACK/server.js
@@ -1,14 +1,34 @@
 import express from "express";
 
+const posts = [
+    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
+];
+
 const app = express();
-app.listen(3000,() =>{ //Esta ouvindo a porta 3000 que é uma porta meio padrão que serviços de servidor local usar
+app.use(express.json());
+
+app.listen(3000, () => { //Esta ouvindo a porta 3000 que é uma porta meio padrão que serviços de servidor local usar
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res)=>{
-    res.status(200).send("Boas vindas à imrersão!");
-}); //definindo a rota
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
 
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)//parametro para pegar o id
+    res.status(200).json(posts[index]); //posts é o arry, index é a posição do objeto
+});
+
+// exercício proposto na primeira aula
 app.get("/livros", (req, res)=>{
     const livro ={
         titulo: 'O Senhor dos Anéis',
@@ -18,3 +38,6 @@ app.get("/livros", (req, res)=>{
     }
     res.json(livro);
 });
+
+
+
